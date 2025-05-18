7524874ae2dbb14bd7c7fe5af993936902077aad
PolnareffTurtle <yechanpark07@gmail.com>
2025-05-17 17:27:23 -0700

    creating a risk map

---
+++ mapping.py
@@ -0,0 +1,19 @@
+import geopandas as gpd
+import pandas as pd
+from geopandas.tools import clip
+from shapely.geometry import box
+import matplotlib.pyplot as plt
+
+
+minx, maxx = -122, -114.0
+miny, maxy = 32.0, 36
+
+counties = gpd.read_file('ca_counties/CA_Counties.shp')
+df = pd.read_csv('county_risks.csv')
+df = df[['COUNTY','RISK_RATNG']]
+counties['risk_rating'] = df['RISK_RATNG']
+
+fig, ax = plt.subplots(figsize=(6, 6))
+counties.plot(column='risk_rating',legend=True,ax=ax, linewidth=1,cmap='OrRd_r')
+plt.title("Risk Rating for Counties in California")
+plt.show()
